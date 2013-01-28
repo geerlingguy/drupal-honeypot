ef31753f14498a698e88bb979a05dfb1800e5af4
Jeff Geerling <geerlingguy@mac.com>
2013-01-28 13:46:07 -0600

    Issue #1778296 by geerlingguy: Add hook in honeypot_add_form_protection.

--- honeypot.api.php
+++ honeypot.api.php
@@ -12,6 +12,41 @@
  */
 
 /**
+ * Alter the honeypot protections added to a particular form.
+ *
+ * @param (array) $options
+ *   Protections that will be applied to the form. May be empty, or may include
+ *   'honeypot' and/or 'time_restriction'.
+ * @param (array) $form
+ *   The Form API form to which protections will be added.
+ */
+function hook_honeypot_form_protections_alter(&$options, $form) {
+  // Add 'time_restriction' protection to 'mymodule-form' if it's not set.
+  if ($form['form_id']['#value'] == 'mymodule_form' && !in_array('time_restriction', $options)) {
+    $options[] = 'time_restriction';
+  }
+}
+
+/**
+ * React to an addition of honeypot form protections for a given form_id.
+ *
+ * After honeypot has added its protections to a form, this hook will be called.
+ * You can use this hook to track when and how many times certain protected
+ * forms are displayed to certain users, or for other tracking purposes.
+ *
+ * @param (array) $options
+ *   Protections that were applied to the form. Includes 'honeypot' and/or
+ *   'time_restriction'.
+ * @param (array) $form
+ *   The Form API form to which protections were added.
+ */
+function hook_honeypot_add_form_protection($options, $form) {
+  if ($form['form_id']['#value'] == 'mymodule_form') {
+    // Do something...
+  }
+}
+
+/**
  * Add time to the Honeypot time limit.
  *
  * In certain circumstances (for example, on forms routinely targeted by
@@ -19,15 +54,15 @@
  * hook to return additional time (in seconds) to honeypot when it is calculates
  * the time limit for a particular form.
  *
- * @param $honeypot_time_limit
+ * @param (int) $honeypot_time_limit
  *   The current honeypot time limit (in seconds), to which any additions you
  *   return will be added.
- * @param $form_values
+ * @param (array) $form_values
  *   Array of form values (may be empty).
- * @param $number
+ * @param (int) $number
  *   Number of times the current user has already fallen into the honeypot trap.
  *
- * @return $additions
+ * @return (int) $additions
  *   Additional time to add to the honeypot_time_limit, in seconds (integer).
  */
 function hook_honeypot_time_limit($honeypot_time_limit, $form_values, $number) {
